99fea9c44facd28e1fcba9d7e48a3889c49d6daa
Spooncodes <spoon.is.god@gmail.com>
2021-11-04 16:05:12 -0400

    added variables

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //Start code
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
+if (passwordLength < 8 || passwordLength > 128){
+  alert("Invalid number of characters")
+  return;
+}
+  var numbers = confirm("Do you want numbers in your password?");
 
+  var lowerCases = confirm("Do you want lowercases in your password?");
+
+  var upperCases = confirm("Do you want uppercases in your password?");
+
+  var special = confirm("Do you want special characters in your password?");
+
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
