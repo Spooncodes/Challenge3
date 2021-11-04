7b4b4b8d06c52f6cb627c5d5b12408102d91470e
Spooncodes <spoon.is.god@gmail.com>
2021-11-04 16:13:09 -0400

    added if statements for array

--- script.js
+++ script.js
@@ -13,7 +13,106 @@ if (passwordLength < 8 || passwordLength > 128){
 
   var special = confirm("Do you want special characters in your password?");
 
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+
+
+  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
   
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
+  
+  
+  // Generator functions**
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+  
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+  
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+  
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+  
+};
+
+// Checks to make sure user selected ok for all and uses empty minimums from above
+
+if (numbers === true) {
+  minimumNumbers = functionArray.getNumbers();
+  minimumCount++;
+
+}
+
+if (lowerCases === true) {
+  minimumLowerCases = functionArray.getLowerCases();
+  minimumCount++;
+
+}
+
+if (upperCases === true) {
+  minimumUpperCases = functionArray.getUpperCases();
+  minimumCount++;
+
+}
+
+if (special === true) {
+  minimumSpecialCharacters = functionArray.getSpecialCharacters();
+  minimumCount++;
+
+}
+
+// empty string variable for the for loop below
+var randomPasswordGenerated = "";
+
+// to make sure characters are added to the password
+randomPasswordGenerated += minimumNumbers;
+randomPasswordGenerated += minimumLowerCases;
+randomPasswordGenerated += minimumUpperCases;
+randomPasswordGenerated += minimumSpecialCharacters;
+
+// loop getting random characters
+for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+
+  // check what type of characters we want --> verify
+  if(numbers === true && randomPasswordGenerated.length < passwordLength) {
+    var randomNumberPicked = functionArray.getNumbers(); // ---> 4
+    randomPasswordGenerated+=randomNumberPicked
+  }
+
+  if(lowerCases === true && randomPasswordGenerated.length < passwordLength) {
+    var randomLowerCase = functionArray.getLowerCases(); // ---> 4
+    randomPasswordGenerated+=randomLowerCase
+  }
+
+  if(upperCases === true && randomPasswordGenerated.length < passwordLength) {
+    var randomUpperCases = functionArray.getUpperCases(); // ---> 4
+    randomPasswordGenerated+=randomUpperCases
+  }
+
+  if(special === true && randomPasswordGenerated.length < passwordLength) {
+    var randomSpecial = functionArray.getSpecialCharacters(); // ---> 4
+    randomPasswordGenerated+=randomSpecial
+  }
+  // get a random character from those types
+}
+
+
+return randomPasswordGenerated;
+
+}
+
+const specialCharacters = "!@#$%^&*()";
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
